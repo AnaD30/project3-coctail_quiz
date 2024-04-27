8d5af300582e773709f242e7efbe0872175d8a25
Ana Duler <ana.duler93@gmail.com>
2024-04-27 17:14:22 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-    SCOPED_CREDS = (Credentials.from_service_account_file('creds.json')).with_scope(SCOPE)
-    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open('coctail_game')
+SCOPED_CREDS = (Credentials.from_service_account_file('creds.json')).with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('coctail_quiz')
 
 def get_sales_data():
     """
@@ -34,6 +34,7 @@ def validate_data(values):
     or if there aren't exactly 6 values. 
     """
     try:
+        [int(value)for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required,you provided {len(values)}"
