9032fdd63ff75211e75828dc507b6066979f8a34
Ana Duler <ana.duler93@gmail.com>
2024-04-27 15:05:42 +0000

    Add validation to check for excatly 6 values in provided data

--- run.py
+++ run.py
@@ -2,15 +2,15 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
-from google.oauth2.service_account import Credentials 
+from google.oauth2.service_account import Credentials
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-    
-    CREDS = Credentials.from_service_account_file('creds.json')
-    SCOPED_CREDS = CREDS.with_scope(SCOPE)
+
+    SCOPED_CREDS = (Credentials.from_service_account_file('creds.json')).with_scope(SCOPE)
     GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
     SHEET = GSPREAD_CLIENT.open('coctail_game')
 
@@ -23,6 +23,23 @@ def get_sales_data():
     print("Example:10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here:")
-    print(f'The data provided is{data_strg}')
+    
+    sales_data = data_str.split(",")
+    print(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if string cannot be convertes int int,
+    or if there aren't exactly 6 values. 
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required,you provided {len(values)}"
+            )
+    except ValueError as e:
+        print("Invalid data: {e},please try again.\n")
+
 
 get_sales_data()
